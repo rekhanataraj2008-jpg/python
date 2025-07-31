661c7b9b703a3be7f3741d2d059e24b1fd1407d3
rekhanataraj2008-jpg <rekhanataraj2008@gmail.com>
2025-07-31 15:00:35 +0530

    Create shopping.py
    shopping program

---
+++ shopping.py
@@ -0,0 +1,7 @@
+shopping.py
+study_meterial=["pencil",'pen','long size']
+for i in study_meterial:
+    print(i)
+for i,meterial in enumerate(study_meterial):
+   print(i+1,meterial)
+   print('end')
